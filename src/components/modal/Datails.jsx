93faa59a6c719de0ac6d0174b09448c3d6400ab0
Diego <diego.ferreira@infracommerce.com.br>
2023-07-09 15:34:27 -0300

    Versão com tela de Detalhes ok

--- src/components/modal/Datails.jsx
+++ src/components/modal/Datails.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
 import { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Details = () => {
   const { identificador } = useParams();
   const [data, setData] = useState(null);
+
+  const formatCurrency = (value) => {
+    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  };
 
   const fetchData = useCallback(async () => {
     try {
@@ -20,22 +25,32 @@ const Details = () => {
   }, [fetchData]);
 
   const Card = ({ item }) => (
-    <div className="col-3" style={{ marginBottom: '20px' }}>
-      <div className="card" style={{ height: '100%' }}>
-        <div className="card-body">
-          <h2 className="card-title">{item.nome}</h2>
-          <p className="card-text">{item.descricao}</p>
-          <p className="card-text">{item.preco}</p>
-          <p className="card-text">{item.quantidade}</p>
+    <div className="d-flex justify-content-center">
+    <div className="col-lg-6 mb-4 border" style={{ marginBottom: '20px', backgroundColor:'#fff', paddingTop:'20px' }}>
+      <Carousel interval={null}>
+        {item.imagens.slice(0, 3).map((imagem, index) => (
+          <Carousel.Item key={index}>
+            <img src={imagem.url} className="d-block w-100" alt={`Imagem ${index}`} style={{ height: '250px' }} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+
+      <div className="card-body">
+        <h2 className="card-title">{item.nome}</h2>
+        <p className="card-text">{item.descricao}</p>
+        <div className="d-flex justify-content-between align-items-center">
+          <p className="card-text">{formatCurrency(item.preco)}</p>
+          <button className="btn btn-success">Comprar</button>
         </div>
       </div>
+    </div>
     </div>
   );
 
   const App = () => {
     return (
-      <div className="app container">
-        <div className="row">
+      <div className="app container d-flex justify-content-center align-items-center">
+      <div className="row">
           {data ? (
             <Card item={data} />
           ) : (
