674a0586fe88f03880e46ac10bbf17974ed09d95
Diego <diego.ferreira@infracommerce.com.br>
2023-07-09 21:35:30 -0300

    Versão com tela de Login e Registro meia boca

--- src/components/modal/Login.jsx
+++ src/components/modal/Login.jsx
@@ -1,7 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-    return (
-        <h1>Login</h1>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = { username, password };
+    const response = await axios.post("https://infracode-api.onrender.com/auth/login", user);
+
+    if (response.status === 200) {
+      localStorage.setItem("isLoggedIn", true);
+      navigate("/success");
+    } else {
+      console.log("Erro ao fazer login");
+        }
   };
-  
-  export default Login;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Usuário: </label>
+      <input type="text" value={username} placeholder="Digite seu usuário" onChange={({ target }) => setUsername(target.value)} required/>
+      <div>
+        <label>Senha: </label>
+        <input type="password" value={password} placeholder="insira uma senha" onChange={({ target }) => setPassword(target.value)} required/>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
--- src/components/modal/Register.jsx
+++ src/components/modal/Register.jsx
@@ -1,50 +1,57 @@
 import { useState } from 'react';
 import  '../../css/Register.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const navigate = useNavigate();
+
+  function mudarNome(e){
+    setNome(e.target.value);
+  }
+
+  function mudarEmail(e){
+    setEmail(e.target.value);
+  }
+
+  function mudarSenha(e){
+    setSenha(e.target.value);
+  }
+
+  async function cadastrar(e) {
+    e.preventDefault(); // Evita a atualização da página
     
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-
-    function mudarNome(e){
-        setNome(e.target.value);
-    }
-
-    function mudarEmail(e){
-        setEmail(e.target.value);
-    }
-
-    function mudarSenha(e){
-        setSenha(e.target.value);
-    }
-
-    function cadastrar(){
-        console.log('Nome: ', nome, "E-mail: ", email, "Senha: ", senha)
-    }
+    console.log('Nome: ', nome, "E-mail: ", email, "Senha: ", senha);
+    const userData = { nome, email, senha };
+    localStorage.setItem('userData', JSON.stringify(userData));
+    const response = await axios.post("https://infracode-api.onrender.com/usuarios", userData);
+    console.log(response);
+    navigate('/login');
+  }
     
-    return (
-        <>
-        <form>
-            <div>
-                <label>Nome:</label>
-                <input type="text" id="nome" name="nome" value={nome} onChange={mudarNome} required /> 
-            </div>
-            <div>
-                <label>E-mail:</label>
-                <input type="email" id="email" name="email" value={email} onChange={mudarEmail} required />
+  return (
+    <>
+      <form onSubmit={cadastrar}>
+        <div>
+          <label>Nome:</label>
+          <input type="text" id="nome" name="nome" value={nome} onChange={mudarNome} required /> 
+        </div>
+        <div>
+          <label>E-mail:</label>
+          <input type="email" id="email" name="email" value={email} onChange={mudarEmail} required />
+        </div>
+        <div>
+          <label>Senha:</label>
+          <input type="password" id="senha" name="senha" value={senha} onChange={mudarSenha} required />
+        </div>
+        <button type="submit"> Cadastrar</button>
+        <p>Já tem uma conta? <Link to="/login"> Entre </Link></p>
+      </form>
+    </>
+  );
+};
 
-            </div>
-            <div>
-                <label>Senha:</label>
-                <input type="password" id="senha" name="senha" value={senha} onChange={mudarSenha} required />
-            </div>
-            <button type="submit" onClick={cadastrar}> Cadastrar</button>
-            <p>Já tem uma conta? <Link to="/login"> Entre </Link></p>
-        </form>
-        </>
-    );
-  };
-  
-  export default Register;
+export default Register;
